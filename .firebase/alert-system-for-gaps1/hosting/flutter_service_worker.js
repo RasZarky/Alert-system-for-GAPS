094296c54c16b7c2e1d918cd691c61ebe1cb0d5d
RasZarky <ubdoolrazak@gmail.com>
2024-08-05 00:38:18 +0000

    add new farmer dialog code changed

--- .firebase/alert-system-for-gaps1/hosting/flutter_service_worker.js
+++ .firebase/alert-system-for-gaps1/hosting/flutter_service_worker.js
@@ -313,9 +313,9 @@ const RESOURCES = {"assets/AssetManifest.bin": "b7b9da7dd8a18cb45cb753373cddacbf
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "e0a9989bb15f055f1764c993accbba49",
-"/": "e0a9989bb15f055f1764c993accbba49",
-"main.dart.js": "78f8badd549427206ccc943c8f313cd1",
+"index.html": "63f6a93e295fe4c65b70da26324882e9",
+"/": "63f6a93e295fe4c65b70da26324882e9",
+"main.dart.js": "b4f2945682d42c01a458ce95625a7ca3",
 "manifest.json": "1e2c4636710e27703c02b7ed679c1143",
 "version.json": "2acab4e26afe7fefc52cba1f410b75f3"};
 // The application shell files that are downloaded before a service worker can
